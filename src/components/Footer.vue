4631880ab8299a7c625d772c4562ea8a50b7fe96
Benedettopana <benedetto.pana@gmail.com>
2024-03-21 17:50:24 +0100

    [add]Bonus footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,44 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      menuInfo: [
+        {
+          title: "Informazioni legali",
+          link: "#",
+        },
+        {
+          title: "Informativa sulla privacy",
+          link: "#",
+        },
+        {
+          title: "Diritto di recesso",
+          link: "#",
+        },
+      ],
+      menuIcon: [
+        {
+          title: "Twitter",
+          link: "#",
+        },
+        {
+          title: "Facebook",
+          link: "#",
+        },
+        {
+          title: "Instagram",
+          link: "#",
+        },
+        {
+          title: "Pinterest",
+          link: "#",
+        },
+        {
+          title: "YT",
+          link: "#",
+        },
+      ],
+    };
   },
 };
 </script>
@@ -15,14 +52,8 @@ export default {
           <!-- nav -->
           <nav class="menu">
             <ul>
-              <li>
-                <a href="#">Informazioni legali</a>
-              </li>
-              <li>
-                <a href="#">Informativa sulla privacy</a>
-              </li>
-              <li>
-                <a href="#">Diritto di recesso</a>
+              <li v-for="(item, index) in menuInfo" :key="index">
+                <a :href="item.link">{{ item.title }}</a>
               </li>
             </ul>
           </nav>
@@ -34,26 +65,26 @@ export default {
           <!-- nav -->
           <nav class="menu">
             <ul>
-              <li>
+              <li v-for="(item, index) in menuIcon" :key="index">
                 <!-- <a href="#"><i class="fa-brands fa-square-twitter"></i></a> -->
-                <a href="#">Twitter</a>
+                <a :href="item.link">{{ item.title }}</a>
               </li>
-              <li>
+              <!-- <li>
                 <a href="#">Facebook</a>
-                <!-- <a href="#"><i class="fa-brands fa-square-facebook"></i></a> -->
+                <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
               </li>
               <li>
                 <a href="#">Instagram</a>
-                <!-- <a href="#"><i class="fa-brands fa-square-instagram"></i></a> -->
+                <a href="#"><i class="fa-brands fa-square-instagram"></i></a>
               </li>
               <li>
                 <a href="#">Pinterest</a>
-                <!-- <a href="#"><i class="fa-brands fa-square-pinterest"></i></a> -->
+                <a href="#"><i class="fa-brands fa-square-pinterest"></i></a>
               </li>
               <li>
                 <a href="#">YT</a>
-                <!-- <a href="#"><i class="fa-brands fa-square-youtube"></i></a> -->
-              </li>
+                <a href="#"><i class="fa-brands fa-square-youtube"></i></a>
+              </li> -->
             </ul>
           </nav>
           <!-- /nav -->
@@ -71,7 +102,7 @@ export default {
   top: calc(100% - 90px);
   width: 100%;
   background-color: $bg-color-bottom-bar;
-  color: white;
+  color: $color-menu-a;
   height: 90px;
   .box {
     margin: 0 17%;
